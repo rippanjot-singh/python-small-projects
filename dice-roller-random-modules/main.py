9daebef22f1807bbbadf264178952c7f2ab5f8c2
Rippanjot <rippanjotsingh90@gmail.com>
2025-10-23 17:05:48 +0530

    minimal-dice-rolling-changes

--- dice-roller-random-modules/main.py
+++ dice-roller-random-modules/main.py
@@ -1,9 +1,11 @@
 import random
 
 while True:
-    roll = input('a. role \nb. no \nAns.')
-    if roll == 'a':
+    roll = input("Press 'enter' to role, Press 'q' to quit")
+    if roll == '':
         print('\033[33m' + str(random.randint(1, 6)) + '\033[0m')
-    else:
-        print('\033[33m' + 'ok' + '\033[0m')
+    elif roll == 'q':
+        print('\033[33m' + 'Thanks for playing!' + '\033[0m')
         break
+    else:
+        print('\033[33m' + 'Invaid input' + '\033[0m')
